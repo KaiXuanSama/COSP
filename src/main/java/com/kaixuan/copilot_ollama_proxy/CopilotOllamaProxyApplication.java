e426fa31ae71f2de650f20aa37774725ac0ac0f1
KaiXuan <2327649721@qq.com>
2026-05-02 18:22:50 +0800

    init: 项目初始化

---
+++ src/main/java/com/kaixuan/copilot_ollama_proxy/CopilotOllamaProxyApplication.java
@@ -0,0 +1,13 @@
+package com.kaixuan.copilot_ollama_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotOllamaProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotOllamaProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaixuan/copilot_ollama_proxy/CopilotOllamaProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaixuan.copilot_ollama_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotOllamaProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
